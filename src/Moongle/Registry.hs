79aa912985a4a8f5b688d5272d90796932d0d454
Hank Hogan <ysh2291939848@outlook.com>
2025-07-31 01:02:52 +0800

    robust package download

--- src/Moongle/Registry.hs
+++ src/Moongle/Registry.hs
@@ -5,9 +5,11 @@
 
 module Moongle.Registry where
 
+import Control.Exception qualified as E
 import Control.Lens
 import Control.Monad
 import Data.Aeson
+import Data.ByteString.Lazy qualified as BL
 import Data.Text qualified as T
 import Data.Vector qualified as V
 import Effectful
@@ -16,15 +18,17 @@ import Effectful.Concurrent.Async
 import Effectful.Error.Static
 import Effectful.FileSystem
 import Effectful.FileSystem.IO.ByteString.Lazy
+import Effectful.Log
 import Effectful.Reader.Static
 import Effectful.Wreq
 import GHC.Generics (Generic)
 import Moongle.Config
 import Moongle.General.Utils
+import Network.HTTP.Client (HttpException)
 import System.FilePath
 import Prelude hiding (writeFile)
 
-data ModuleInfo = ModuleInfo {_name :: T.Text, _version :: T.Text, _description :: T.Text}
+data ModuleInfo = ModuleInfo {_name :: T.Text, _version :: T.Text, _description :: Maybe T.Text}
   deriving (Show, Generic)
 
 instance FromJSON ModuleInfo where
@@ -40,39 +44,59 @@ instance FromJSON Registry where
 
 makeLenses ''Registry
 
-fetchRegistry :: (Wreq :> es, Reader Config :> es) => Eff es Registry
+fetchRegistry :: (Wreq :> es, Reader Config :> es, Log :> es) => Eff es Registry
 fetchRegistry = do
-  regUrl <- T.unpack <$> asks (^. registryUrl)
+  regtext <- asks (^. registryUrl)
+  let regUrl = T.unpack regtext
+  logInfo_ $ "Fetching registry from: " <> regtext
   resp <- get regUrl
-  reg <- asJSON resp
-  pure $ reg ^. responseBody
+  reg <- (^. responseBody) <$> asJSON resp
+  logInfo_ $ "Successfully fetched registry with " <> T.pack (show (V.length $ reg ^. modules)) <> " modules"
+  pure reg
 
-downloadPackage :: (Wreq :> es, FileSystem :> es, Reader Config :> es, Error String :> es) => (T.Text, T.Text, T.Text) -> Eff es ()
+downloadPackage :: (Wreq :> es, FileSystem :> es, Reader Config :> es, Error String :> es, Log :> es) => (T.Text, T.Text, T.Text) -> Eff es ()
 downloadPackage (uname, pname, ver) = do
   lp <- T.unpack <$> asks (^. moongleStoragePath)
   baseurl <- asks (^. mooncakesBaseUrl)
+  let tag = uname <> "-" <> pname <> "-" <> ver
   let url = T.unpack $ baseurl <> "/" <> uname <> "/" <> pname <> "/" <> ver <> ".zip"
   let destName = T.unpack $ pname <> "-" <> ver
-  let destDir = lp </> "packages" </> destName
-  createDirectoryIfMissing True destDir
-  resp <- (^. responseBody) <$> get url
-  contents <- case unZip resp of
-    Left err -> throwError_ $ "Failed to unzip module: " <> err
-    Right files -> pure files
-  forM_ contents $ \(rawPath, bytes) -> do
-    let rel = normalise rawPath
-    let out = destDir </> rel
-    createDirectoryIfMissing True (takeDirectory out)
-    writeFile out bytes
+  let destDir = lp </> "packages" </> T.unpack uname </> destName
 
-fetchAll :: (Wreq :> es, FileSystem :> es, Reader Config :> es, Error String :> es, Concurrent :> es) => Eff es ()
+  already <- doesDirectoryExist destDir
+  if already
+    then do
+      logInfo_ $ T.pack destDir <> " already exists, skipping download for " <> tag
+    else do
+      respE <- getWith opts url
+      case respE ^. responseStatus . statusCode of
+        200 -> do
+          logInfo_ $ "Module downloaded successfully: " <> uname <> "-" <> pname <> "-" <> ver
+          let resp = (^. responseBody) respE
+
+          contents <- either (throwError_ . ("Failed to unzip module" <>)) pure (unZip resp)
+
+          forM_ contents $ \(rawPath, bytes) -> do
+            let rel = normalise rawPath
+            let out = destDir </> rel
+            createDirectoryIfMissing True (takeDirectory out)
+            writeFile out bytes
+        s -> logInfo_ $ "Module failed to download: " <> uname <> "-" <> pname <> "-" <> ver <> "with status code: " <> T.pack (show s)
+
+fetchAll :: (Error String :> es, Reader Config :> es, Wreq :> es, FileSystem :> es, Concurrent :> es, Log :> es) => Eff es ()
 fetchAll = do
   Registry mods <- fetchRegistry
   maxThreads <- asks (^. parallel)
   when (maxThreads <= 0) $ throwError_ @String "parallelism must be positive"
-  pooledForConcurrentlyN_ maxThreads (V.toList mods) downloadOne
+  pooledForConcurrentlyN_ maxThreads mods downloadOne
   where
     downloadOne ModuleInfo {..} =
       case T.splitOn "/" _name of
-        [u, p] -> downloadPackage (u, p, _version)
+        u : xs -> downloadPackage (u, mconcat xs, _version)
         _ -> throwError_ $ "Malformed module name: " <> T.unpack _name
+
+safeGet :: (IOE :> es, Wreq :> es) => String -> Eff es (Either HttpException (Response BL.ByteString))
+safeGet url = withRunInIO (\run -> E.try @HttpException (run (get url)))
+
+opts :: Effectful.Wreq.Options
+opts = defaults & checkResponse ?~ (\_ _ -> pure ())
--- test/Main.hs
+++ test/Main.hs
@@ -1,13 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Data.ByteString.Lazy qualified as BL
-import Moongle.General.Utils
+import Effectful
+import Effectful.Concurrent
+import Effectful.Error.Static
+import Effectful.FileSystem
+import Effectful.Log
+import Effectful.Reader.Static
+import Effectful.Wreq
+import Log.Backend.StandardOutput
+import Moongle.Config
+import Moongle.Registry
+import Control.Lens
+
+-- main :: IO ()
+-- main = do
+--   raw <- BL.readFile "0.2.0.zip"
+--   let result = unZip raw
+--   case result of
+--     Left err -> putStrLn $ "Error: " ++ show err
+--     Right fs -> do
+--       print $ fst <$> fs
+
+testConfig :: Config
+testConfig =
+  Config
+    { _registryUrl = "https://mooncakes.io/assets/modules.json",
+      _moongleStoragePath = "/Users/hank/.moongle",
+      _mooncakesBaseUrl = "https://moonbitlang-mooncakes.s3.us-west-2.amazonaws.com/user",
+      _parallel = 32
+    }
+
+runTest :: Eff '[Concurrent, FileSystem, Wreq, Reader Config, Log, Error String, IOE] () -> IO ()
+runTest action = do
+  a <- runEff $ withStdOutLogger $ \stdoutLogger ->
+    runErrorNoCallStack $ runLog "demo" stdoutLogger defaultLogLevel $ runReader testConfig $ runWreq $ runFileSystem $ runConcurrent action
+  case a of
+    Left err -> putStrLn $ "Error: " ++ err
+    Right _ -> putStrLn "Test completed successfully"
+
 
 main :: IO ()
-main = do
-  raw <- BL.readFile "0.2.0.zip"
-  let result = unZip raw
-  case result of
-    Left err -> putStrLn $ "Error: " ++ show err
-    Right fs -> do
-      print $ fst <$> fs
+main = runTest fetchAll
