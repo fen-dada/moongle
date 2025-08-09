10fde3b9e9a44db781c48370aedbfbcbe4f98c96
Hank Hogan <ysh2291939848@outlook.com>
2025-08-09 22:46:18 +0800

    implemented typesearch

--- src/Moongle/Server.hs
+++ src/Moongle/Server.hs
@@ -10,9 +10,10 @@ import Moongle.Server.Handler
 import Moongle.Server.Types
 import Network.Wai.Handler.Warp as Warp
 import Servant qualified as S
+import Effectful.PostgreSQL
 
-serverT :: (Reader Env :> es, Error S.ServerError :> es, Log :> es) => S.ServerT MoongleAPI (Eff es)
+serverT :: (Reader Env :> es, Error S.ServerError :> es, Log :> es, IOE :> es, WithConnection :> es) => S.ServerT MoongleAPI (Eff es)
 serverT = searchHandler S.:<|> statsHandler
 
-server :: (Reader Env :> es, Log :> es, IOE :> es) => Eff es ()
+server :: (Reader Env :> es, Log :> es, IOE :> es, WithConnection :> es) => Eff es ()
 server = runWarpServerSettings @MoongleAPI Warp.defaultSettings serverT
